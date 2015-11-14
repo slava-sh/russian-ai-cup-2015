de329eda352bedd87057175211e0a87a1c97e1c0
Slava Shklyaev <shk.slava@gmail.com>
2015-11-14 19:04:13 +0200

    Implement QuickStartGuy

--- src/main/java/MyStrategy.java
+++ src/main/java/MyStrategy.java
@@ -3,15 +3,44 @@ import model.Game;
 import model.Move;
 import model.World;
 
+import static java.lang.StrictMath.*;
+
 public final class MyStrategy implements Strategy {
     @Override
     public void move(Car self, World world, Game game, Move move) {
-        move.setEnginePower(1.0D);
-        move.setThrowProjectile(true);
-        move.setSpillOil(true);
+        double nextWaypointX = (self.getNextWaypointX() + 0.5D) * game.getTrackTileSize();
+        double nextWaypointY = (self.getNextWaypointY() + 0.5D) * game.getTrackTileSize();
 
-        if (world.getTick() > game.getInitialFreezeDurationTicks()) {
-            move.setUseNitro(true);
+        double cornerTileOffset = 0.25D * game.getTrackTileSize();
+
+        switch (world.getTilesXY()[self.getNextWaypointX()][self.getNextWaypointY()]) {
+            case LEFT_TOP_CORNER:
+                nextWaypointX += cornerTileOffset;
+                nextWaypointY += cornerTileOffset;
+                break;
+            case RIGHT_TOP_CORNER:
+                nextWaypointX -= cornerTileOffset;
+                nextWaypointY += cornerTileOffset;
+                break;
+            case LEFT_BOTTOM_CORNER:
+                nextWaypointX += cornerTileOffset;
+                nextWaypointY -= cornerTileOffset;
+                break;
+            case RIGHT_BOTTOM_CORNER:
+                nextWaypointX -= cornerTileOffset;
+                nextWaypointY -= cornerTileOffset;
+                break;
+            default:
+        }
+
+        double angleToWaypoint = self.getAngleTo(nextWaypointX, nextWaypointY);
+        double speedModule = hypot(self.getSpeedX(), self.getSpeedY());
+
+        move.setWheelTurn(angleToWaypoint * 32.0D / PI);
+        move.setEnginePower(0.75D);
+
+        if (speedModule * speedModule * abs(angleToWaypoint) > 2.5D * 2.5D * PI) {
+            move.setBrake(true);
         }
     }
 }
